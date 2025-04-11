0931b16e0295e61caac715116bead6ec0ac7c875
HUUTUAN <nguyenhuutuan2k1@gmail.com>
2025-04-12 01:05:51 +0700

    chore: update Firebase configuration to use environment variables

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -8,14 +8,14 @@ import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyC9bgVeJT9q3QNOWGFyKHOytE5COG3JqEA",
-	authDomain: "garden-esp8266.firebaseapp.com",
-	databaseURL: "https://garden-esp8266-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "garden-esp8266",
-	storageBucket: "garden-esp8266.firebasestorage.app",
-	messagingSenderId: "385387298522",
-	appId: "1:385387298522:web:f03b2e8a7dc9a119cdefe3",
-	measurementId: "G-K269PF492F"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
       };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
